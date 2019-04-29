3048339209a3f9bd71f76085766cf6836974030b
flipwalls <1134549391@qq.com>
2019-04-29 15:24:24 +0800

    first the1

---
+++ wp-admin/post-new.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://www.zsblog.cn/wp-login.php?redirect_to=https%3A%2F%2Fwww.zsblog.cn%2Fwp-admin%2Fpost-new.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://www.zsblog.cn/wp-login.php?redirect_to=https%3A%2F%2Fwww.zsblog.cn%2Fwp-admin%2Fpost-new.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://www.zsblog.cn/wp-login.php?redirect_to=https%3A%2F%2Fwww.zsblog.cn%2Fwp-admin%2Fpost-new.php&reauth=1">https://www.zsblog.cn/wp-login.php?redirect_to=https%3A%2F%2Fwww.zsblog.cn%2Fwp-admin%2Fpost-new.php&reauth=1</a></p>
+	</body>
+</html>
---
+++ wp-login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="zh_CN">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="zh_CN">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; 紫苏博客 &#8212; WordPress</title>
+	<link rel='stylesheet' id='dashicons-css'  href='/wp-includes/css/dashicons.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='/wp-includes/css/buttons.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='/wp-admin/css/forms.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='/wp-admin/css/l10n.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='/wp-admin/css/login.min.css?ver=5.1.1' type='text/css' media='all' />
+<link rel="stylesheet" id="wp-admin-css" href="/wp-content/themes/mkBlog/css/admin-style.css" type="text/css" /><style>.login h1 a {background-image: url(https://zsblog.cn/wp-content/uploads/2019/03/20190325_010514_10.png);}</style>	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="/wp-content/uploads/2019/03/cropped-20190325_010521_43-1-32x32.png" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2019/03/cropped-20190325_010521_43-1-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2019/03/cropped-20190325_010521_43-1-180x180.png" />
+<meta name="msapplication-TileImage" content="https://www.zsblog.cn/wp-content/uploads/2019/03/cropped-20190325_010521_43-1-270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress">Powered by WordPress</a></h1>
+	
+	<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p>Login form protected by <a href='http://www.bad-neighborhood.com/login-lockdown.html' rel='nofollow'>Login LockDown</a>.<br /><br /><br /></p>	<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+				<input type="hidden" name="redirect_to" value="https://www.zsblog.cn/wp-admin/post-new.php" />
+					<input type="hidden" name="testcookie" value="1" />
+	</p>
+	</form>
+
+			<p id="nav">
+			<a href="/wp-login.php?action=register">Register</a> | 		<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+				</p>
+	
+	<script type="text/javascript">
+	function wp_attempt_focus(){
+	setTimeout( function(){ try{
+			d = document.getElementById('user_login');
+				d.focus();
+	d.select();
+	} catch(e){}
+	}, 200);
+	}
+
+			wp_attempt_focus();
+			if(typeof wpOnload=='function')wpOnload();
+			</script>
+
+			<p id="backtoblog"><a href="/">
+		&larr; Back to 紫苏博客	</a></p>
+		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="/copyright-html/">免责声明</a></div>	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
+<!--
+Performance optimized by W3 Total Cache. Learn more: https://www.w3-edge.com/products/
+
+Object Caching 23/23 objects using memcached
+Page Caching using memcached (Requested URI is rejected) 
+Database Caching using memcached (Request-wide Request URI is rejected)
+
+Served from: zsblog.cn @ 2019-04-29 14:29:49 by W3 Total Cache
+-->
